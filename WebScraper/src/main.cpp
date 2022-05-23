e3005515b249ec6d13454dc5ae16d1422419c62a
MFRSiam <mfrforhad1@gmail.com>
2022-05-24 01:40:15 +0600

    Added A New Function

--- WebScraper/src/main.cpp
+++ WebScraper/src/main.cpp
@@ -6,9 +6,12 @@
 #include <vector>
 
 
+struct MemoryStruct {
+	char* memory;
+	size_t size;
+};
 
-
-size_t gotData(char *buffer,size_t itemSize,size_t numberOfItems,void *ignorThis = nullptr) {\
+size_t gotData(char *buffer,size_t itemSize,size_t numberOfItems, void* userp=nullptr) {\
 	fmt::print(fmt::fg(fmt::color::blue), "\nChunk Starting\n");
 	size_t bytes = itemSize * numberOfItems;
 	fmt::print("New Chunk Size {}\n", bytes);
@@ -21,6 +24,7 @@ size_t gotData(char *buffer,size_t itemSize,size_t numberOfItems,void *ignorThis
 			fmt::print("{}\t", lineNumber);
 		}
 	}
+
 	fmt::print(fmt::fg(fmt::color::red), "Chunk Ending\n");
 	std::string data(buffer);
 	return bytes;
@@ -36,18 +40,21 @@ int main() {
 		fmt::print("Error Occured");
 		return EXIT_FAILURE;
 	}
+	struct MemoryStruct chunk;
 
 	//Set Actions 
 	CURLcode res;
 	curl_easy_setopt(curl, CURLOPT_URL, "https://www.4chan.org/");
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, gotData);
-	
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&chunk);
 
 	res = curl_easy_perform(curl);
+	fmt::print("{} bytes retrieved\n", (unsigned long)chunk.size);
+
+	std::string data(chunk.memory);
 	
 
-	//Perform Our Actions
-
 	curl_easy_cleanup(curl);
+	curl_global_cleanup();
 	return EXIT_SUCCESS;
 }
