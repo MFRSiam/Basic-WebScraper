9902d6d8f34b4061e065f50b4bf4058f4d6bf616
MFRSiam <mfrforhad1@gmail.com>
2022-06-15 11:46:54 +0600

    Re-Org 0.0.2

--- WebScraper/src/Scrapper.h
+++ WebScraper/src/Scrapper.h
@@ -9,16 +9,30 @@
 class Scrapper
 {
 private:
+    // Private Variables
     std::string url;
     std::vector<std::string> embededUrl;
     bool urlStatus;
     int responseStatus;
     std::vector<std::string> downloadTypes;
+
+    // Private Functions
+
 public:
     Scrapper();
     Scrapper(std::string url);
 
     std::vector<std::string> getAncorTags();
+    void insertTypeToLookFor(std::string type);
+
+
+    void initiateDownload();
+    void initiateDownload(std::string path);
     
+
+    int getNumberOfAncorTags();
+    int getNumberOfRequiredTags();
+
+
 };
 
